6dbd3e682207eb5bb9648d1997665cf599821eca
zalaps <zalaps@hotmail.com>
2015-04-30 19:04:39 +0530

    Entities & Letsstart Class defined
    refactoring round 2

---
+++ MongoConsole/MongoConsole/MongoPlay/LetsStart.cs
@@ -0,0 +1,108 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoConsole.MongoPlay
+{
+    public class LetsStart
+    {
+        public string ConnString { get; set; }
+        public string DBName { get; set; }
+        public MongoServer mServer { get; set; }
+        public MongoDatabase mDB { get; set; }
+
+        public LetsStart(string connString, string dbname)
+        {
+            this.ConnString = connString;
+            this.DBName = dbname;
+
+            var mongoClient = new MongoClient(this.ConnString);
+            this.mServer = mongoClient.GetServer();
+        }
+
+        /* So, what is hierarchy?
+         * Client
+         *      Server
+         *          DataBase
+         *              Collection
+         *                  Document
+         */
+
+        public void CreateDB()
+        {
+            /* Here server.GetDatabase method will create DB if not exist 
+             * Same is not true when creating colletion inside db. will result in exception. */
+            Console.WriteLine("--> Creating csharp db");
+            this.mDB = mServer.GetDatabase(this.DBName);
+            Console.WriteLine("--> DB Name: {0}", this.mDB.Name);
+        }
+
+        public void GetAllDBNames()
+        {
+            /* Get list of all database names */
+            Console.WriteLine("--> List of database:");
+            var dbs = new List<string>(mServer.GetDatabaseNames());
+            foreach (var db in dbs)
+            {
+                Console.WriteLine(db);
+                /* csharp db not in the list? it wont come till first collection is created.
+                 * skip to next section - create collection - come back here to get printed! */
+            }
+        }
+
+        public void DropDB()
+        {
+            /* Get list of all database names */
+            Console.WriteLine("--> List of database:");
+            var dbs = new List<string>(mServer.GetDatabaseNames());
+            foreach (var db in dbs)
+            {
+                Console.WriteLine(db);
+                /* csharp db not in the list? it wont come till first collection is created.
+                 * skip to next section - create collection - come back here to drop database! */
+                if (db == "csharp")
+                {
+                    Console.WriteLine("--> Deleting database: {0}", db);
+                    /* Drop database  */
+                    var dbToDrop = mServer.GetDatabase(db);
+                    dbToDrop.Drop();
+                }
+            }
+        }
+
+        public void CreateCollections()
+        {
+            if(this.mDB == null)
+                this.mDB = mServer.GetDatabase(this.DBName);
+
+            /* Collection: It is 'collection' of documnets */
+            Console.WriteLine("--> Create database collection");
+            CommandResult result = this.mDB.CreateCollection("customer");
+            if (result.Ok)
+                Console.WriteLine("--> Colletion created successfully");
+            else
+                Console.WriteLine("--> Error: {0}", result.ErrorMessage);
+
+            /* Create another collection. 
+             * These will be used later. */
+            this.mDB.CreateCollection("survey");
+            this.mDB.CreateCollection("surveyfeedback");
+        }
+
+        public void GetAllCollectionFromDB()
+        {
+            if (this.mDB == null)
+                this.mDB = mServer.GetDatabase(this.DBName);
+
+            /* Get collection list from DB. */
+            Console.WriteLine("--> Get database collection from DB: {0}", mDB.Name);
+            foreach (string name in mDB.GetCollectionNames())
+            {
+                Console.WriteLine(name);
+            }
+        }        
+    }
+}
--- MongoConsole/MongoConsole/Program.cs
+++ MongoConsole/MongoConsole/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using MongoConsole.MongoPlay;
 
 namespace MongoConsole
 {
@@ -17,61 +18,15 @@ namespace MongoConsole
             string connString = "mongodb://localhost";
             string dbname = "csharp";
 
-            /* So, what is hierarchy?
-             * Client
-             *      Server
-             *          DataBase
-             *              Collection
-             *                  Document
-             */
-
-            Console.WriteLine(">> Creating csharp db");
-            var mongoClient = new MongoClient(connString);
-            var mongoServer = mongoClient.GetServer();
-
-            /* Here server.getdatabase method will create DB if not exist 
-             * Same is not true when creating colletion inside db. will result in exception. 
-             */
-            var mongoDB = mongoServer.GetDatabase(dbname);
-            Console.WriteLine(">> DB Name: {0}", mongoDB.Name);
-
-            /* Get list of all database names */
-            Console.WriteLine(">> List of database:");
-            var dbs = new List<string>(mongoServer.GetDatabaseNames());
-            foreach (var db in dbs)
-            {
-                Console.WriteLine(db);
-                /* csharp db not in the list? it wont come till first collection is created.
-                 * skip to next section - create collection - come back here to drop database
-                 */
-                if (db == "csharp")
-                {
-                    Console.WriteLine(">> Deleting database: {0}", db);
-                    /* Drop database  */
-                    var dbToDrop = mongoServer.GetDatabase(db);
-                    // dbToDrop.Drop();
-                }
-            }
-
-            /* Collection: It is 'collection' of documnets */
-            Console.WriteLine(">> Create database collection");
-            CommandResult result = mongoDB.CreateCollection("customer");
-            if (result.Ok)
-                Console.WriteLine(">> Colletion created successfully");
-            else
-                Console.WriteLine(result.ErrorMessage);
-
-            /* Create another collection. */
-            mongoDB.CreateCollection("survey");
-            mongoDB.CreateCollection("surveyfeedback");
-
-            /* Get collection list from DB. */
-            Console.WriteLine(">> Get database collection");
-            foreach (string name in mongoDB.GetCollectionNames())
-            {
-                Console.WriteLine(name);
-            }
-
+            /* Instantiating start object */
+            var letsStart = new LetsStart(connString, dbname);
+            
+            letsStart.CreateDB();
+            letsStart.GetAllDBNames();
+            letsStart.DropDB();
+            letsStart.CreateCollections();
+            letsStart.GetAllCollectionFromDB();        
+          
         }
     }
 }
